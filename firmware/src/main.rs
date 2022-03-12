2e4d27dd0a93bd50a2ece00aa7b62bc919d53e93
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2022-03-12 09:08:24 +0100

    queue/heapless/no interrupt..

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -15,6 +15,8 @@
 
 mod config;
 
+// Define
+mod panic;
 
 // The macro for our start-up function
 use cortex_m_rt::entry;
@@ -30,7 +32,7 @@ use embedded_time::rate::*;
 
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
-use panic_halt as _;
+// use panic_halt as _;
 
 // Pull in any important traits
 use rp_pico::hal::prelude::*;
@@ -55,13 +57,12 @@ static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
 /// The USB Bus Driver (shared with the interrupt).
 static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
 
+
 /// The USB Serial Device Driver (shared with the interrupt).
 static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
 
 
-use const_queue::ConstQueue;
-
-use heapless::consts::*;
+// use heapless::consts::*;
 use serde::{Serialize, Deserialize};
 use serde_json_core;
 
@@ -73,9 +74,16 @@ struct Point {
 }
 
 
+use heapless::spsc::Queue;
 
-static mut qqq: Option<ConstQueue::<[u8; 200], 3>> = None;
+// Notice, type signature needs to be explicit for now.
+// (min_const_eval, does not allow for default type assignments)
+static mut QQQ: Queue<[u8; 100], 4> = Queue::new();
 
+
+
+static mut CmdBuffer: [u8; 100] = [0; 100];
+static mut CmdBufferIdx: u32 = 0;
 
 
 /// Entry point to our bare-metal application.
@@ -88,9 +96,6 @@ static mut qqq: Option<ConstQueue::<[u8; 200], 3>> = None;
 #[entry]
 fn main() -> ! {
 
-    unsafe {
-        qqq = Some( ConstQueue::<[u8; 200], 3>::new() );
-    }
 
     // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
@@ -177,20 +182,24 @@ fn main() -> ! {
     let mut led_pin = pins.led.into_push_pull_output();
 
 
-    // let mut q = ConstQueue::<i32, 3>::new();
-    // let _ = q.push(10);
-    // let _ = q.push(20);
-    // assert!(q.pop().unwrap() == 10);
-    // assert!(q.pop().unwrap() == 20);
-
+    // NOTE(unsafe) beware of aliasing the `consumer` end point
+    let mut consumer = unsafe { QQQ.split().1 };
 
     // Blink the LED at 1 Hz
     loop {
-        cortex_m::interrupt::free(|_| {
-            unsafe { 
-                // COUNTER += 1 
-            };
-        });
+        // `dequeue` is a lockless operation
+        let cmd = consumer.dequeue();
+        if cmd != None {
+            {
+                cortex_m::interrupt::free(|_| {
+                    unsafe {
+                        let serial = USB_SERIAL.as_mut().unwrap();
+                        let _ = serial.write(b"{ \"debug\": \"looop\" }\r\n");
+                    }
+                })
+            }
+        }
+            
 
         led_pin.set_high().unwrap();
         delay.delay_ms(500);
@@ -200,7 +209,21 @@ fn main() -> ! {
 }
 
 
-
+/// convert int n into a u8 buffer buf
+fn base_10_bytes(mut n: u8, buf: &mut [u8]) -> &[u8] {
+    if n == 0 {
+        return b"0";
+    }
+    let mut i = 0;
+    while n > 0 {
+        buf[i] = (n % 10) as u8 + b'0';
+        n /= 10;
+        i += 1;
+    }
+    let slice = &mut buf[..i];
+    slice.reverse();
+    &*slice
+}
 
 
 /// This function is called whenever the USB Hardware generates an Interrupt
@@ -220,12 +243,15 @@ unsafe fn USBCTRL_IRQ() {
 
     let point = Point { x: 1, y: 2 };
     let serialized = serde_json_core::to_string::<Point, 100>(&point);
+
     
+    // NOTE(unsafe) beware of aliasing the `producer` end point
+    let mut producer = unsafe { QQQ.split().0 };
 
 
     // Poll the USB driver with all of our supported USB Classes
     if usb_dev.poll(&mut [serial]) {
-        let mut buf = [0u8; 64];
+        let mut buf = [0u8; 128];
         match serial.read(&mut buf) {
             Err(_e) => {
                 // Do nothing
@@ -235,16 +261,51 @@ unsafe fn USBCTRL_IRQ() {
             }
             Ok(count) => {
 
-                // let _ = serial.write(b"{ \"debug\": \"Hello!\" }\r\n");
-
-                let _ = serial.write(serialized.unwrap().as_bytes());
-                serial.write(b"\r\n");
-
-                
                 
 
-                // serde_json_core::ser::to_string();
 
+                
+                // static mut CmdBuffer: [u8; 100] = [0; 100];
+                // static mut CmdBufferIdx: u32 = 0;
+                
+                // let _ = serial.write(serialized.unwrap().as_bytes());
+                
+
+                for i in 0..count {
+                    
+                    let c = buf[i];
+
+                    let mut bbbb=[0u8;4];
+                    let oo = base_10_bytes(c, &mut bbbb);
+
+                    let mut bbbb2=[0u8;4];
+                    let oo8 = base_10_bytes('\n' as u8, &mut bbbb2);
+
+                    serial.write(&[c]).unwrap();
+                    serial.write(b" ").unwrap();
+                    serial.write(&oo).unwrap();
+                    serial.write(b" ").unwrap();
+                    serial.write(&oo8).unwrap();
+                    serial.write(b" \r\n").unwrap();
+
+                    if c == 'z' as u8 {
+                        let _ = serial.write(b"{ \"debug\": \"Hello!\" }\r\n");
+                    }
+
+                    let mm = b"{ \"debug\": \"Hello return\" }\r\n";
+                    for n in 0..mm.len() {
+                        CmdBuffer[n] = mm[n];
+                    }
+                    
+
+                    if c == '\r' as u8 || c == '\n' as u8 {
+                        let _ = serial.write(b"{ \"debug\": \"Hello return\" }\r\n");
+
+                        producer.enqueue(CmdBuffer).ok().unwrap();
+                    }
+                    
+                }
+                
                 // // Convert to upper case
                 // buf.iter_mut().take(count).for_each(|b| {
                 //     b.make_ascii_uppercase();
---
+++ firmware/src/panic.rs
@@ -0,0 +1,12 @@
+
+
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+
