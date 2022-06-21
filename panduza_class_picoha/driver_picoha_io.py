2ae39e4fc0a5436fc8f2875af4321dfdcf9a4058
xdoctorwhoz <xavier.rodriguezalvarez@gmail.com>
2022-06-21 21:54:28 +0200

    test the idea of using statemachine in the driver

--- panduza_class_picoha/driver_picoha_io.py
+++ panduza_class_picoha/driver_picoha_io.py
@@ -5,70 +5,84 @@ import pyudev
 import serial
 from loguru import logger
 from panduza_platform import MetaDriverIo
+from statemachine import StateMachine, State
+
+# pip install python-statemachine
+
+# -----------------------------------------------------------------------------
+
+class CallbackState(State):
+    def __init__(self, name, cb, value=None, initial=False):
+        super().__init__(name, value, initial)
+        self.cb = cb
+
+# -----------------------------------------------------------------------------
+
+
+# class TrafficLightMachine(StateMachine):
+#     green = State('Green', initial=True)
+#     yellow = State('Yellow')
+#     red = State('Red')
+
+#     slowdown = green.to(yellow)
+#     stop = yellow.to(red)
+#     go = red.to(green)
+    
+
+# traffic_light = TrafficLightMachine()
+
+# print(traffic_light.current_state)
+
+# traffic_light.slowdown()
+
+# print(traffic_light.current_state)
+
+
+
+
+
+
 
 class DriverPicohaIO(MetaDriverIo):
-    """ Driver twi Master
+    """Driver IO for the Picoha
     """
 
     ###########################################################################
     ###########################################################################
     
     def config(self):
-        """ FROM MetaDriver
+        """FROM MetaDriver
         """
         return {
             "compatible": "picoha_twi_master",
-            "info": { "type": "twi/master", "version": "1.0" },
+            "info": { "type": "io", "version": "1.0" },
             "settings": {
-                "usbid_vendor": "[optional] Usb vendor ID in the following format (\"16c0\" : default)",
+                "usbid_vendor":  "[optional] Usb vendor ID in the following format (\"16c0\" : default)",
                 "usbid_product": "[optional] Usb product ID in the following format (\"05e1\" : default)",
-                "usbid_serial": "[optional] Usb serial ID",
+                "usbid_serial":  "[optional] Usb serial ID",
             }
         }
-
-        # "bitrate_hz": "spi bitrate as an integer in hz (4000000)",
-        # "clock_polarity": "CPOL [0 / 1]",
-        # "clock_phase": "CPHA [0 / 1]",
-        # "bitorder": "[msb / lsb] first",
-        # "ss_polarity": "[active_low / active_high]"
 
     ###########################################################################
     ###########################################################################
 
     def setup(self, tree):
-        """ FROM MetaDriver
+        """FROM MetaDriver
         """
         # Initialize properties
         self.usbid_vendor = "16c0"
         self.usbid_product = "05e1"
         self.usbid_serial = None
 
-        # # Get serial port
-        # self.serial_port = self.ttyPortfromUsbInfo(self.usbid_vendor, self.usbid_product, self.usbid_serial, base_dev_tty="/dev/ttyACM")
-        # if self.serial_port is None:
-        #     raise Exception(f"Serial Not Found")
-
-        # # Serial found continue
-        # logger.debug(f"serial port: {self.serial_port}")
-
-        # Register commands
-        # self.register_command("data/read", self.__data_read)
-        # self.register_command("data/write", self.__data_write)
-        # self.register_command("data/writeRead", self.__data_write_read)
+        # Get serial port
+        self.serial_port = self.ttyPortfromUsbInfo(self.usbid_vendor, self.usbid_product, self.usbid_serial, base_dev_tty="/dev/ttyACM")
+        if self.serial_port is None:
+            raise Exception(f"Serial Not Found")
 
         #
         self.ser = serial.Serial(self.serial_port, 115200)
 
-        # # Get bitrate
-        # self.bitrate_khz = 1000
-        # if "bitrate_hz" in tree["settings"]:
-        #     self.bitrate_khz = int(tree["settings"]["bitrate_hz"] / 1000)
 
-        # #
-        # logger.debug(f"bitrate: {self.bitrate_khz}khz")
-
-        # #
-        # AardvarkBridge.ConfigureTwiMaster(self.aa_handle, self.bitrate_khz)
 
         #
         logger.debug(f"interface ready")
@@ -78,7 +92,7 @@ class DriverPicohaIO(MetaDriverIo):
     ###########################################################################
 
     def on_start(self):
-        """ From MetaDriver
+        """From MetaDriver
         """
         pass
         
@@ -86,98 +100,34 @@ class DriverPicohaIO(MetaDriverIo):
     ###########################################################################
 
     def loop(self):
-        """ FROM MetaDriver
+        """FROM MetaDriver
         """
         return False
 
-    ###########################################################################
-    ###########################################################################
-
-    # def __data_write(self, payload):
-    #     """
-    #     """
-    #     # Debug log
-    #     logger.debug(f"CMD data/write ({payload})")
-
-        # # Parse the params
-        # req = self.payload_to_dict(payload)
-        # data = base64.b64decode(req["data"])
-        # addr = req["addr"]
-        
-        # flags = AA_I2C_NO_FLAGS
-        # if "addr_10b" in req and req["addr_10b"]:
-        #     flags = flags | AA_I2C_10_BIT_ADDR
-        # if "no_stop" in req and req["no_stop"]:
-        #     flags = flags | AA_I2C_NO_STOP
-
-        # #
-        # status = aa_i2c_write(self.aa_handle, addr, flags, array('B', data) )
-        # if status < 0:
-        #     print(f"fail sending data ({aa_status_string(status)})")
-        #     return
-    
-        # logger.debug(f"CMD data/read ({payload})")
-
-
-    ###########################################################################
-    ###########################################################################
-
-    # def __data_read(self, payload):
-    #     """
-    #     """
-    #     # Debug log
-    #     logger.debug(f"CMD data/read ({payload})")
-
-
-    ###########################################################################
-    ###########################################################################
-
-    # def __data_write_read(self, payload):
-    #     """
-    #     """
-    #     # Debug log
-    #     logger.debug(f"CMD data/writeRead ({payload})")
-
-    #     # Parse the params
-    #     req = self.payload_to_dict(payload)
-    #     data = base64.b64decode(req["data"])
-    #     addr = req["addr"]
-        
-    #     #
-    #     srequest = {
-    #         "cmd": "twi_rw"
-    #     }
-
-    #     #
-    #     srequest_str = json.dumps(srequest) + '\r\n'
-    #     self.ser.write(srequest_str.encode())
-
-    #     #
-    #     line = self.ser.readline()
-    #     print(line, "\n")
-
     ###############################################################################
     ###############################################################################
 
-    # def ttyPortfromUsbInfo(self, vendor_id, product_id, serial=None, base_dev_tty="/dev/ttyACM"):
-    #     # Explore usb device with tty subsystem
-    #     udev_context = pyudev.Context()
-    #     for device in udev_context.list_devices(ID_BUS='usb', SUBSYSTEM='tty'):
-    #         properties = dict(device.properties)
+    def ttyPortfromUsbInfo(self, vendor_id, product_id, serial=None, base_dev_tty="/dev/ttyACM"):
+        """Find tty port from usb information
+        """
+        # Explore usb device with tty subsystem
+        udev_context = pyudev.Context()
+        for device in udev_context.list_devices(ID_BUS='usb', SUBSYSTEM='tty'):
+            properties = dict(device.properties)
             
-    #         # Need to find the one with the DEVNAME corresponding to the /dev serial port
-    #         if 'DEVNAME' not in properties or not properties['DEVNAME'].startswith(base_dev_tty):
-    #             continue
+            # Need to find the one with the DEVNAME corresponding to the /dev serial port
+            if 'DEVNAME' not in properties or not properties['DEVNAME'].startswith(base_dev_tty):
+                continue
 
-    #         # Check vendor/product/serial
-    #         if vendor_id == properties["ID_VENDOR_ID"] and product_id == properties["ID_MODEL_ID"]:
-    #             if serial:
-    #                 if serial == properties["ID_SERIAL_SHORT"]:
-    #                     return properties["DEVNAME"]
-    #             else:
-    #                 return properties["DEVNAME"]
+            # Check vendor/product/serial
+            if vendor_id == properties["ID_VENDOR_ID"] and product_id == properties["ID_MODEL_ID"]:
+                if serial:
+                    if serial == properties["ID_SERIAL_SHORT"]:
+                        return properties["DEVNAME"]
+                else:
+                    return properties["DEVNAME"]
 
-    #     return None
+        return None
 
     ###############################################################################
     ###############################################################################
